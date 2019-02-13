2c7049855fe74edf4277e9d84355ec2cc6e69211
Jan Zahradník <jan.zahradnik@data3s.com>
2019-02-13 09:29:26 +0100

    fix unused constructor parameter

--- src/Spring/Spring.Core/Context/Support/XmlApplicationContext.cs
+++ src/Spring/Spring.Core/Context/Support/XmlApplicationContext.cs
@@ -1,7 +1,7 @@
 #region License
 
 /*
- * Copyright © 2002-2011 the original author or authors.
+ * Copyright ï¿½ 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -242,7 +242,7 @@ namespace Spring.Context.Support
             bool caseSensitive,
             IApplicationContext parentContext,
             params string[] configurationLocations)
-            : this(new XmlApplicationContextArgs(name, parentContext, configurationLocations, null, true, refresh))
+            : this(new XmlApplicationContextArgs(name, parentContext, configurationLocations, null, caseSensitive, refresh))
         { }
 
 
